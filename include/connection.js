2a595099eb3147a58f1e26e63e09b927e14f368f
Daniel Panyin Mensah <78688545+Daniel024419@users.noreply.github.com>
2023-07-07 22:16:43 +0000

    Update connection.js

--- include/connection.js
+++ include/connection.js
@@ -8,6 +8,7 @@ var con = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    multipleStatements: true,
 });
 
 //  var con = mysql.createConnection({
